3f7e4447880275beb392bcfa67b039a8b03677c3
Ishwar Govind <ishwar.govind@live.com>
2022-12-02 18:55:20 +0530

    small update to PPO

--- src/agents/PPO.py
+++ src/agents/PPO.py
@@ -6,11 +6,16 @@ import time
 import numpy as np
 import pandas as pd
 import sys
-sys.path.append("..")
-from envs.StockTrading import StockEnv
+import seaborn as sns
+
+if True:
+    sys.path.append("..")
+    from envs.StockTrading import StockEnv
+
+sns.set_theme()
 
 tickers = ['BNDX', 'URTH']
-start_date = '2019-01-01'
+start_date = '2012-01-01'
 end_date = "2022-11-20"
 df = pdr.get_data_yahoo([tickers][0], start=start_date, end=end_date)
 
@@ -22,17 +27,17 @@ data['Adj Close'] = data['Adj Close'].bfill()
 df = df.bfill(axis=1)
 data = data['Adj Close']
 
-train_pct = 0.8
+train_pct = 0.5
 samples_train = int(train_pct*len(data))
-#print(len(data))
+# print(len(data))
 data_train = data[:samples_train]
 data_test = data[samples_train:]
 
 max_trade = 30
-balance = 0
+balance = 10000
 transaction_fee = 0.001
 
-plt.rcParams["figure.figsize"] = (10,6)
+plt.rcParams["figure.figsize"] = (10, 6)
 for i in tickers:
     plt.plot(data[i], label=i)
 
@@ -41,16 +46,17 @@ plt.legend()
 plt.margins(x=0)
 plt.ylabel("Closing Share Prise (USD)")
 plt.xlabel("Time (Years)")
-plt.savefig('StockPrice.png',bbox_inches='tight')
+plt.savefig('StockPrice.png', bbox_inches='tight')
 
-
-runs = 3
+# start training and predicting
+runs = 5
 timesteps = 10000
+
 
 def train():
     policy = "MlpPolicy"
     train_env = DummyVecEnv([lambda: StockEnv(df=data_train)])
-    model = PPO(policy, train_env, verbose=1, seed=42)
+    model = PPO(policy, train_env, verbose=1)
     start = time.time()
     model.learn(total_timesteps=timesteps)
     end = time.time()
@@ -63,13 +69,13 @@ def predict():
     actions_memory = []
     env = DummyVecEnv([lambda: StockEnv(df=data_test)])
     obs = env.reset()
-    ppo.set_random_seed(42)
     for i in range(len(data_test.index.unique())):
         action, _states = ppo.predict(obs)
         obs, rewards, dones, info = env.step(action)
         if i == (len(data_test.index.unique()) - 2):
             actions_memory = env.env_method(method_name="get_action_memory")
     return actions_memory[0]
+
 
 length = data_test.shape[0]
 stocks = data_test.shape[1]
@@ -87,6 +93,9 @@ while (i < runs):
     Cumulative_returns_daily_drl_ppo[i] = (1+return_stocks_ppo).cumprod()
     i = i+1
 
+
+# plotting the results
+
 fig, axs = plt.subplots(2, 2, figsize=(17, 10), constrained_layout=True)
 
 ppo_mean = np.mean(np.array(Cumulative_returns_daily_drl_ppo), axis=0)
@@ -97,6 +106,7 @@ axs[0, 0].fill_between(data_test.index, ppo_mean - ppo_std,
                        ppo_mean + ppo_std, alpha=0.2, color='tab:blue')
 axs[0, 0].margins(x=0)
 axs[0, 0].margins(y=0)
+axs[0, 0].set_ylim(0)
 axs[0, 0].axhline(1, color='black', linestyle='--', lw=2)
 axs[0, 0].set_ylabel("Cumulative Returns")
 axs[0, 0].set_xlabel("Time (Years-Months)")
